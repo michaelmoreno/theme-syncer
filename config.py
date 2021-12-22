20ef05017c7ca9982a7b2cfdda2868244aaf6ea3
Michael Moreno <michaelmoreno.dev@gmail.com>
2021-12-22 12:09:24 -0700

    Setup wallpaper & keyboard RGB syncing and usage checks

---
+++ config.py
@@ -0,0 +1,37 @@
+# Program to change keyboard backlight color
+rgb_controller = '~/rogauracore/rogauracore'
+# Program to change wallpaper
+wallpaper_controller = 'feh --bg-scale'
+
+wallpaper_path = '~/Pictures/wallpapers'
+
+themes = {
+    'theme_1': {
+        'wallpaper': f'{wallpaper_path}/bluedear.jpg',
+        'kb_rgb': 'cyan',
+    },
+    'theme_2': {
+        'wallpaper': f'{wallpaper_path}/redmountains.jpg',
+        'kb_rgb': 'red',
+    },
+    'theme_3': {
+        'wallpaper': f'{wallpaper_path}/green1.jpg',
+        'kb_rgb': 'green',
+    },
+    'theme_4': {
+        'wallpaper': f'{wallpaper_path}/purpleplanets.jpg',
+        'kb_rgb': 'magenta',
+    },
+    'theme_5': {
+        'wallpaper': f'{wallpaper_path}/orangeblossom.jpg',
+        'kb_rgb': 'red',
+    },
+    'theme_6': {
+        'wallpaper': f'{wallpaper_path}/pinktree.jpg',
+        'kb_rgb': 'magenta',
+    },
+    'theme_7': {
+        'wallpaper': f'{wallpaper_path}/redblossom.jpg',
+        'kb_rgb': 'red',
+    },
+}
---
+++ theme-syncer.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import config as cfg
+import random
+
+working_dir = sys.path[0]
+
+def set_theme(theme):
+    os.system(f'{cfg.wallpaper_controller} {cfg.themes[f"theme_{theme}"]["wallpaper"]} && sudo {cfg.rgb_controller} {cfg.themes[f"theme_{theme}"]["kb_rgb"]}')
+
+
+def cycle_theme():
+    with open(f'{working_dir}/current_theme.txt', 'r') as f:
+        current = int(f.read())
+        if current == len(cfg.themes):
+            current = 0
+        set_theme(current+1)
+    with open(f'{working_dir}/current_theme.txt', 'w') as f:
+        f.write(str(current+1))
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 0 or args[0] == 'help':
+        print('||========== THEME SYNCER ==========||\n'
+              'Usage:   python theme-syncer.py <arg>\n'
+              'Arguments:'
+              '\n\tcycle\tSwitch to next theme'
+              '\n\trandom\tSwitch to random theme'
+              '\n\t<num>\tSwitch to nth theme (e.g 1)')
+        sys.exit()
+        
+    if args[0].isdigit():
+        if len(args) > 1:
+            print('Error: Too many arguments'
+            '\nUsage: python theme-syncer.py <arg>')
+            sys.exit()
+        if not 0 < int(args[0]) <= len(cfg.themes):
+            print(f'Error:  Theme number out of range (1-{len(cfg.themes)})')
+            sys.exit()
+
+        set_theme(args[0])
+        
+    elif args[0] == 'cycle':
+        cycle_theme()
+        
+    elif args[0] == 'random':
+        set_theme(random.randint(1, len(cfg.themes)))
+
+    
+if __name__ == '__main__':
+    main()
